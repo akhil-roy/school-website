1747d6533a2c8d7acd74c84fbb174a19ad4bfd56
Akhil Roy <65934234+akhil-roy@users.noreply.github.com>
2024-12-03 13:14:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 
-app.config.from_object('config.DevConfig')
+app.config.from_object('config.ProdConfig')
 
 login_manager = LoginManager()
 login_manager.init_app(app)
